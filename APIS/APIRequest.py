2e37e69265be0308ef09845d67898814fb7be836
Gustavo Dantas <dantas2509@gmail.com>
2020-09-12 15:22:23 -0300

    Update APIRequest.py
    fix:  correct minor bugs in code

--- APIS/APIRequest.py
+++ APIS/APIRequest.py
@@ -5,15 +5,14 @@ import datetime as dt
 
 class WebServiceSafra:
 
-    def __init__(self, client_id='f9d3cd9600874ac2803d03ca709b78eb',
-                 client_secret='1a2075e3-b15e-4324-902c-0f12f8f08082', accountID=''):
+    def __init__(self, client_id='70a973924bdd4defb211bfd1c0309771',
+                 client_secret='51971ed5-9704-4757-924a-d3431a2ae60d', accountid='00711234522'):
         self.url = 'https://af3tqle6wgdocsdirzlfrq7w5m.apigateway.sa-saopaulo-1.oci.customer-oci.com/fiap-sandbox'
         self.token = self.getToken(client_id, client_secret)
-        self.accountID = accountID
+        self.accountID = accountid
         assert self.verifyUp(), 'Service Unvailable. Try again later.'
 
-    @staticmethod
-    def getToken(client_id, client_secret):
+    def getToken(self, client_id, client_secret):
         from base64 import b64encode
 
         urlToken = 'https://idcs-902a944ff6854c5fbe94750e48d66be5.identity.oraclecloud.com/oauth2/v1/token'
@@ -32,7 +31,7 @@ class WebServiceSafra:
             'authorization': f'Bearer {self.token}'
         }
         resp = requests.request('GET', self.url + path, headers=headers)
-        return resp.status_code == 200
+        return str(resp.status_code == 200)
 
     def getAccountData(self):
         path = f'/open-banking/v1/accounts/{self.accountID}'
@@ -94,14 +93,17 @@ class WebServiceSafra:
 
         resp = requests.request('POST', self.url + path, headers=headers, data=data)
 
-        return resp.status_code
+        return 'Succeed' if resp.status_code else 'Failed'
 
-    def listMorningCalls(self, fromDate=(dt.datetime.today() - dt.timedelta(days=7)).strftime('%Y-%m-%d'),
-                         toDate=dt.datetime.today().strftime('%Y-%m-%d')):
+    def listMorningCalls(self, fromDate='2020-07-09', toDate='2020-07-14'):
         path = f'/media/v1/youtube?fromData={fromDate}&toData={toDate}&playlist=morningCalls&channel=safra'
         headers = {
             'authorization': f'Bearer {self.token}'
         }
         resp = requests.request('GET', self.url + path, headers=headers)
         return resp.json()
+
+
+newAccount = WebServiceSafra()
+print(newAccount.listMorningCalls())
 
