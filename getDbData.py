e79c50fb7e480007028760ece5fd6ba0e31476d0
Gustavo Dantas <dantas2509@gmail.com>
2020-09-13 15:25:11 -0300

    feat: route to validate login get transactions

--- getDbData.py
+++ getDbData.py
@@ -3,18 +3,19 @@ import pyodbc
 class dbData:
     def __init__(self, server='localhost\SQLEXPRESS01', db='SafraHubDB', usr='technee', pwd='123456'):
         self.conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                      f'Server={server};'
-                      f'Database={db};'
-                      f'UID={usr};'
-                      f'PWD={pwd};')
+                                   f'Server={server};'
+                                   f'Database={db};'
+                                   f'UID={usr};'
+                                   f'PWD={pwd};')
         self.cursor = self.conn.cursor()
 
-    def selectData(self, table, filters='', columns='*', order='', group=''):
+    def selectData(self, table, filters='', columns='*', order='', group='', join=''):
         sel = f'SELECT {columns} FROM {table} '
+        joi = join + ' '
         fil = f'WHERE {filters} ' if len(filters) > 0 else ' '
         ord = f'ORDER BY {order} ' if len(order) > 0 else ' '
         grp = f'GROUP BY {group} ' if len(group) > 0 else ' '
 
-        print(sel + fil + ord + grp)
-        self.cursor.execute(sel + fil + ord + grp)
+        # print(sel + joi + fil + ord + grp)
+        self.cursor.execute(sel + joi + fil + ord + grp)
         return [row for row in self.cursor]
--- router.py
+++ router.py
@@ -1,12 +1,16 @@
 from flask import Flask, request
 from flask_cors import CORS
+from APIS import APIRequest
 import os
+import getDbData
+import json
 
 from APIS import APIRequest
 
 app = Flask(__name__)
 app._static_folder = os.path.abspath('APIS/')
 CORS(app)
+
 
 @app.route('/', methods=['GET'])
 def isUp():
@@ -90,6 +94,75 @@ def morningCalls():
     """localhost:8080/morningcalls"""
     session = APIRequest.WebServiceSafra()
     return session.listMorningCalls()
+
+
+'''------------------------------------------------ DATABASE ROUTES ------------------------------------------------'''
+
+
+@app.route('/validlogin', methods=['POST'])
+def validLogin():
+    """
+    localhost:8080/validlogin
+    body = {
+            "id": "1",
+            "pwd":"hsasyagsyag"
+            }
+    """
+    token = APIRequest.WebServiceSafra().getToken()
+    profile = list()
+
+    data = request.json
+    base = getDbData.dbData()
+    # User Data
+    ret = base.selectData('AppAccount', f"customer_id = {data['id']} and customer_password_hash = '{data['pwd']}'")
+    if len(ret) == 0:
+        return 'Unauthorized'
+    else:
+        profile.append({'customer_id': ret[0][0], 'customer_name': ret[0][3], 'customer_email': ret[0][1], 'token': token,'banks': []})
+        # Bank Accounts
+        ret = base.selectData('Bank', f"customer_id = {data['id']}", columns='Bank.bank_id, Bank.bank_name, acc.Account_id',
+                              join='INNER JOIN '
+                                   'BankAccount acc '
+                                   'on Bank.bank_id '
+                                   '= '
+                                   'acc.Account_bank_id')
+        profile[0]['banks'] = [{'bank_id': x[0], 'bank_name': x[1], 'bank_account': x[2]} for x in ret]
+
+        return json.dumps(profile[0])
+
+
+@app.route('/transactionsdb', methods=['GET'])
+def transactionDB():
+    """
+    localhost:8080/transactionsDB
+    body = {
+            "account_id": "1",
+            }
+    """
+    profile = list()
+
+    data = request.json
+    base = getDbData.dbData()
+    # User Data
+    ret = base.selectData('Transactions', f"account_id = {data['account_id']}")
+    if len(ret) == 0:
+        return 'None'
+    else:
+        transactions = [{
+            'transaction_id': x[0],
+            'account_id': x[1],
+            'transaction_booking_date': x[2],
+            'transaction_date': x[3],
+            'transaction_recipient_account': x[4],
+            'transaction_receipt': x[5],
+            'transaction_credit_debit_indicator': x[6],
+            'transaction_currency': x[7],
+            'transaction_amount': int(x[8]),
+            'transaction_description': x[9],
+            'transaction_status': x[10]
+        } for x in ret]
+
+        return json.dumps(transactions[0])
 
 
 if __name__ == '__main__':
