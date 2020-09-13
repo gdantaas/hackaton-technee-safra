d1f8afd423506b7caebe5a71cb1bb2e746ff8d0a
Gustavo Dantas <dantas2509@gmail.com>
2020-09-13 14:03:20 -0300

    feat: Database Connection

---
+++ getDbData.py
@@ -0,0 +1,20 @@
+import pyodbc
+
+class dbData:
+    def __init__(self, server='localhost\SQLEXPRESS01', db='SafraHubDB', usr='technee', pwd='123456'):
+        self.conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+                      f'Server={server};'
+                      f'Database={db};'
+                      f'UID={usr};'
+                      f'PWD={pwd};')
+        self.cursor = self.conn.cursor()
+
+    def selectData(self, table, filters='', columns='*', order='', group=''):
+        sel = f'SELECT {columns} FROM {table} '
+        fil = f'WHERE {filters} ' if len(filters) > 0 else ' '
+        ord = f'ORDER BY {order} ' if len(order) > 0 else ' '
+        grp = f'GROUP BY {group} ' if len(group) > 0 else ' '
+
+        print(sel + fil + ord + grp)
+        self.cursor.execute(sel + fil + ord + grp)
+        return [row for row in self.cursor]
